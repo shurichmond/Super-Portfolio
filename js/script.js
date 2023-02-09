efe2dc81a4f1db8f7354d47dcbb4fc54cc6ebd37
Shu Nfortayong Richmond <72896458+shurichmond@users.noreply.github.com>
2023-02-09 14:13:09 +0100

    Add js file and modified menu button

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const navlinks = document.querySelector('.navlinks');
+const menu = document.querySelector('.fa-bars');
+const close = document.querySelector('.fa-x');
+// console.log(menu)
+
+menu.addEventListener('click', () => {
+  navlinks.classList.add('open')
+  close.classList.add('show')
+})
+
+close.addEventListener('click', () => {
+  navlinks.classList.remove('open')
+  close.classList.remove('show')
+})
