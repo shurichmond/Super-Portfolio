2e3ab27b2bb6e54c4ad088c7d15e6b9c4eae98e5
Shu Nfortayong Richmond <72896458+shurichmond@users.noreply.github.com>
2023-03-30 16:41:38 +0100

    Modified js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const navlinks = document.querySelector('.navlinks');
 const menu = document.querySelector('.fa-bars');
 const close = document.querySelector('.fa-x');
+const ulLinks = document.querySelector('links');
 // console.log(menu)
 
 menu.addEventListener('click', () => {
@@ -12,3 +13,7 @@ close.addEventListener('click', () => {
   navlinks.classList.remove('open');
   close.classList.remove('show');
 });
+
+ulLinks.addEventListener('click', () => {
+  
+}) 
